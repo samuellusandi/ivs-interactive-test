d9199b261725584c83e3d9638c1b154d57d41fb4
TimusVPF <tenkoirigo@gmail.com>
2021-04-20 10:54:16 +0900

    Init commit

---
+++ src/index.js
@@ -0,0 +1,71 @@
+const dotenv = require('dotenv');
+const AWS = require('aws-sdk');
+const cors = require('cors');
+const express = require('express');
+
+dotenv.config();
+
+const env = process.env;
+const awsKey = env.AWS_KEY;
+const awsSecret = env.AWS_SECRET;
+const awsRegion = env.AWS_REGION;
+
+const creds = new AWS.Credentials({
+    accessKeyId: awsKey,
+    secretAccessKey: awsSecret,
+})
+const ivs = new AWS.IVS({
+    endpoint: `https://ivs.${awsRegion}.amazonaws.com`,
+    credentials: creds,
+});
+const port = +env.APP_PORT;
+const arn = env.AWS_CHANNEL_ARN;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+
+app.get('/', (req, res) => {
+    res
+        .status(200)
+        .send({
+            message: 'Hello, world!'
+        });
+});
+
+app.post('/emoji', (req, res) => {
+    const body = req.body;
+    if (body && Object.keys(body).length !== 0) {
+        ivs.putMetadata({
+            channelArn: arn,
+            metadata: JSON.stringify(body),
+        }, (err, data) => {
+            if (err) {
+                res.status(400).send({
+                    data: {
+                        success: false,
+                        error: err.code,
+                    }
+                 });
+            } else {
+                res.status(200).send({
+                    data: {
+                        success: true,
+                    }
+                });
+            }
+        });
+        return;
+    }
+    res.status(200).send({
+        data: {
+            success: false,
+            error: 'No body provided',
+        },
+    });
+});
+  
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
